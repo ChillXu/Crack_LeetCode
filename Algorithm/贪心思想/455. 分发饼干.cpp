210d5daf73923e46281e6122dc09a7deb9fdd46a
ChillXu <1587440752@qq.com>
2021-11-10 21:59:20 +0800

    Update 455. 分发饼干.cpp

--- Algorithm/贪心思想/455. 分发饼干.cpp
+++ Algorithm/贪心思想/455. 分发饼干.cpp
@@ -20,3 +20,27 @@ public:
         return num;
     }
 };
+
+
+//
+//解法二，在while循环一直j++，一但出循环说明有满足的，则计数count++，j则在外循环继续自增
+//
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int numOfChildren = g.size(), numOfCookies = s.size();
+        int count = 0;
+        for (int i = 0, j = 0; i < numOfChildren && j < numOfCookies; i++, j++) {
+            while (j < numOfCookies && g[i] > s[j]) {
+                j++;
+            }
+            if (j < numOfCookies) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
