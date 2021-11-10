5761ab81994b3bfe7417e1718e69be08081afacd
ChillXu <1587440752@qq.com>
2021-11-10 21:34:43 +0800

    Update 455. 分发饼干.cpp

--- Algorithm/贪心思想/455. 分发饼干.cpp
+++ Algorithm/贪心思想/455. 分发饼干.cpp
@@ -1,1 +1,22 @@
-####
+//
+//贪心先排序，外循环小朋友，先把胃口小的满足了，这样饼干数组只能继续向后遍历
+//因为小朋友的胃口是逐渐增大的，只有后面的更大的饼干才能满足
+//
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int num=0,index=0;
+        for(int i=0;i<g.size();++i){
+            for(int j=index;j<s.size();++j){
+                if(g[i]<=s[j]){
+                    num++;
+                    index=j+1;
+                    break;    //应该是break,不是continue！！
+                }
+            }
+        }
+        return num;
+    }
+};
