8b3d19df51db8024d78f33f90fb896b5b112bffc
ChillXu <1587440752@qq.com>
2022-02-22 13:38:37 +0800

    Update 142. 环形链表 II.cpp

--- Algorithm/双指针/142. 环形链表 II.cpp
+++ Algorithm/双指针/142. 环形链表 II.cpp
@@ -1,3 +1,11 @@
 \\
 \\
 \\
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}    //类构造函数初始化数据成员（非静态const成员）
+ * };
+ */
