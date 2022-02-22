b9f62c22686e686ced08c8f1e96a31c19c57a0f0
ChillXu <1587440752@qq.com>
2022-02-22 11:32:09 +0800

    Update 167. 两数之和 II.cpp

--- Algorithm/双指针/167. 两数之和 II.cpp
+++ Algorithm/双指针/167. 两数之和 II.cpp
@@ -1,1 +1,19 @@
 \\
+\\numbers数组已经排序，只需要方向相反的双指针来遍历
+\\
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l=0,r=numbers.size()-1;
+        int sum;
+        while(l<r){
+            sum=numbers[l]+numbers[r];
+            if(sum==target){
+                break;
+            }
+            else if(sum<target){++l;}
+            else --r;
+        }
+        return vector<int>{l+1,r+1};  \\直接能够返回容器
+    }
+};
