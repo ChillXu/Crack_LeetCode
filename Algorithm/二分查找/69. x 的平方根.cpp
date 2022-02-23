4f81a6dac5543e973ced48220b16bfe331a89ae2
ChillXu <1587440752@qq.com>
2022-02-23 15:39:02 +0800

    Update 69. x 的平方根.cpp

--- Algorithm/二分查找/69. x 的平方根.cpp
+++ Algorithm/二分查找/69. x 的平方根.cpp
@@ -21,5 +21,16 @@ public:
 };
 
 \\
-\\牛顿迭代法
+\\牛顿迭代法 y=x^2-a=0
+\\过x0做y的切线，y=f(x0)+f'(x0)(x-x0) -> x=x0-f(x0)/f'(x0)
 \\
+class Solution {
+public:
+    int mySqrt(int a) {
+        long x=a;
+        while(x*x>a){
+            x=(x+a/x)/2;
+        }
+        return x;
+    }
+};
