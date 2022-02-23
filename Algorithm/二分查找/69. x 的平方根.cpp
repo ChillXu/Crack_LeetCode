4620c78aaf95c83a10cd15eb6ae0636cc9373d8b
ChillXu <1587440752@qq.com>
2022-02-23 11:55:18 +0800

    Update 69. x 的平方根.cpp

--- Algorithm/二分查找/69. x 的平方根.cpp
+++ Algorithm/二分查找/69. x 的平方根.cpp
@@ -1,3 +1,25 @@
 \\
+\\二分查找
 \\
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=0,r=x,mid;
+        int ans;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if((long long)mid*mid<=x){      //用乘法需要考虑溢出情况
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+\\
+\\牛顿迭代法
 \\
