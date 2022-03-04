bf34cd2d3b4200f542b6b2437e3772be0a9cbc40
ChillXu <1587440752@qq.com>
2022-03-04 16:18:40 +0800

    Update 232. 用栈实现队列.cpp

--- Algorithm/数据结构/232. 用栈实现队列.cpp
+++ Algorithm/数据结构/232. 用栈实现队列.cpp
@@ -1,3 +1,49 @@
 \\
 \\
 \\
+class MyQueue {
+    stack<int> in,out;
+    void in2out(){
+        while(!in.empty()){
+            int x=in.top();
+            in.pop();
+            out.push(x);
+        }
+    }
+public:
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        if(out.empty()){
+            in2out();
+        }
+        int x=out.top();
+        out.pop();
+        return x;
+    }
+    
+    int peek() {
+        if(out.empty()){
+            in2out();
+        }
+        return out.top();
+    }
+    
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
