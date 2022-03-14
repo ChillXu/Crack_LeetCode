5b6977ad113d66cc0f95ba101412345c72d521d0
ChillXu <1587440752@qq.com>
2022-03-15 00:11:18 +0800

    Update 34. 在排序数组中查找元素的第一个和最后一个位置.cpp

--- Algorithm/二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
+++ Algorithm/二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -1,6 +1,6 @@
-\\
-\\熟悉lower_bound和upper_bound函数
-\\
+//
+//熟悉lower_bound和upper_bound函数
+//
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
@@ -9,7 +9,7 @@ public:
         }
         int lower=lower_bound(nums,target);
         int upper=upper_bound(nums,target)-1;
-        if(lower==nums.size()||nums[lower]!=target){  \\前一个条件target过大，后一个target过小，查询不到
+        if(lower==nums.size()||nums[lower]!=target){  //前一个条件target过大，后一个target过小，查询不到
             return vector<int>{-1,-1};
         }
         return vector<int>{lower,upper};
@@ -18,11 +18,11 @@ public:
         int l=0,r=nums.size(),mid;
         while(l<r){
             mid=l+(r-l)/2;
-            if(nums[mid]>=target){    \\ >= 若nums[mid]等于target，右指仍然针会一直更新
+            if(nums[mid]>=target){    // >= 若nums[mid]等于target，右指仍然针会一直更新
                 r=mid;  //左闭右开
             }
             else{
-                l=mid+1;     \\不断更新l，直到到达r的位置
+                l=mid+1;     //不断更新l，直到到达r的位置
             }
         }
         return l;
@@ -31,7 +31,7 @@ public:
         int l=0,r=nums.size(),mid;
         while(l<r){
             mid=l+(r-l)/2;
-            if(nums[mid]>target){    \\ > 右指针会停在大于target的位置
+            if(nums[mid]>target){    // > 右指针会停在大于target的位置
                 r=mid;  //左闭右开
             }
             else{
