3125da76270b02d5f2f89b50fa550b231f0d32d3
ChillXu <1587440752@qq.com>
2022-03-14 17:23:00 +0800

    Update 547. 省份数量.cpp

--- Algorithm/优先搜索/547. 省份数量.cpp
+++ Algorithm/优先搜索/547. 省份数量.cpp
@@ -1,3 +1,26 @@
 \\
+\\用visited记录已访问的城市，主函数完成一次dfs即为一个省
 \\
-\\
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size(), count=0;
+        vector<bool> visited(n,false);
+        for(int i=0;i<n;++i){
+            if(!visited[i]){
+                dfs(isConnected,i,visited);
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    void dfs(vector<vector<int>>& isConnected, int i, vector<bool>& visited){
+        visited[i]=true;
+        for(int k=0;k<isConnected.size();++k){
+            if(isConnected[i][k]==1 && !visited[k]){
+                dfs(isConnected,k,visited);
+            }
+        }
+    }
+};
