e7c60a142305b01188b552dcef40d4c0f9cd1bc1
ChillXu <1587440752@qq.com>
2022-03-14 23:36:58 +0800

    Update 46. 全排列.cpp

--- Algorithm/优先搜索/46. 全排列.cpp
+++ Algorithm/优先搜索/46. 全排列.cpp
@@ -1,3 +1,25 @@
 \\
 \\回溯法
+\\input: [1,2,3]
+\\output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 \\
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        backtracking(nums,0,ans);
+        return ans;
+    }
+
+    void backtracking(vector<int>& nums, int level,vector<vector<int>>& ans){   \\level记录递归的层
+        if(level==nums.size()-1){
+            ans.push_back(nums);
+            return;                                                              \\return很关键！！！
+        }
+        for(int i=level;i<nums.size();++i){
+            swap(nums[level],nums[i]);
+            backtracking(nums,level+1,ans);
+            swap(nums[level],nums[i]);
+        }
+    }
+};
