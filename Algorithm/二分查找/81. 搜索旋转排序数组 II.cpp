ab5a119fc373d062ef10214314bcd7ddadcd8c6b
ChillXu <1587440752@qq.com>
2022-03-15 00:12:14 +0800

    Update 81. 搜索旋转排序数组 II.cpp

--- Algorithm/二分查找/81. 搜索旋转排序数组 II.cpp
+++ Algorithm/二分查找/81. 搜索旋转排序数组 II.cpp
@@ -1,13 +1,13 @@
-\\
-\\nums=[3,1,2,3,3,3,3]
-\\nums=[3,3,3,3,1,2,3] 无法判断左边有序还是右边有序
-\\
+//
+//nums=[3,1,2,3,3,3,3]
+//nums=[3,3,3,3,1,2,3] 无法判断左边有序还是右边有序
+//
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int start=0,end=nums.size()-1;
         int mid;
-        while(start<=end){  \\ "="很关键，最终判断 
+        while(start<=end){  // "="很关键，最终判断 
             mid=(start+end)/2;
             if(nums[mid]==target){
                 return true;
@@ -16,7 +16,7 @@ public:
                 ++start;
             }
             else if(nums[start]<nums[mid]){
-                if(nums[mid]>target && nums[start]<=target){   \\ target需要在整个区间内，左右都要判断
+                if(nums[mid]>target && nums[start]<=target){   // target需要在整个区间内，左右都要判断
                     end=mid-1;
                 }
                 else{
