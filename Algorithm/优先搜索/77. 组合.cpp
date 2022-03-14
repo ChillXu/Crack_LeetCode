6f0714cd6185a4392b209a3facdeb1e27e4f0c4d
ChillXu <1587440752@qq.com>
2022-03-15 00:07:44 +0800

    Update 77. 组合.cpp

--- Algorithm/优先搜索/77. 组合.cpp
+++ Algorithm/优先搜索/77. 组合.cpp
@@ -1,3 +1,24 @@
-\\
-\\
-\\
+//
+//组合回溯
+//
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> comb(k,0);
+        backtracking(comb,0,n,k,1,ans);   //pos初始值为1！
+        return ans;
+    }
+
+    void backtracking(vector<int>& comb, int count, int n, int k, int pos, vector<vector<int>>& ans){
+        if(count==k){
+            ans.push_back(comb);
+            return;
+        }
+        for(int i=pos;i<=n;++i){
+            comb[count++]=i;
+            backtracking(comb,count,n,k,i+1,ans);   //i+1不是pos+1,写成后者在回溯的时候无法正确添加之后的数组元素到comb
+            count--;
+        }
+    }
+};
