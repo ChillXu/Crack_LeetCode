5cced08bea7f527a1b4c14813ce90bd924681aa2
ChillXu <1587440752@qq.com>
2022-03-15 00:13:59 +0800

    Update 76. 最小覆盖子串.cpp

--- Algorithm/双指针/76. 最小覆盖子串.cpp
+++ Algorithm/双指针/76. 最小覆盖子串.cpp
@@ -1,24 +1,24 @@
-\\
-\\数组映射字符
-\\
+//
+//数组映射字符
+//
 class Solution {
 public:
     string minWindow(string s, string t) {
         vector<int> chars(128,0);
         vector<bool> flags(128,false);
-        for(int i=0;i<t.size();++i){      \\统计t中的字符情况
+        for(int i=0;i<t.size();++i){      //统计t中的字符情况
             flags[t[i]]=true;
             ++chars[t[i]];
         }
         int l=0,r,min_len=s.size()+1,cnt=0,start;
-        for(r=0;r<s.size();++r){        \\移动右指针r
+        for(r=0;r<s.size();++r){        //移动右指针r
             if(flags[s[r]]){
-                if(--chars[s[r]]>=0){    \\满足说明t中的字符还未全部覆盖
+                if(--chars[s[r]]>=0){    //满足说明t中的字符还未全部覆盖
                     cnt++;         
                 }
             }
             while(cnt==t.size()){
-                if(r-l+1<min_len){     \\可能上一轮的r也产生了一个最小覆盖
+                if(r-l+1<min_len){     //可能上一轮的r也产生了一个最小覆盖
                     min_len=r-l+1;
                     start=l;
                 }
@@ -33,6 +33,6 @@ public:
 };
 
 
-\\
-\\hash映射
-\\
+//
+//hash映射
+//
