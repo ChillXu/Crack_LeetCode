4396c9f62c64d7db1f9ee2f462be10b932cb5774
ChillXu <1587440752@qq.com>
2022-03-15 00:16:24 +0800

    Update 448. 找到所有数组中消失的数字.cpp

--- Algorithm/数据结构/448. 找到所有数组中消失的数字.cpp
+++ Algorithm/数据结构/448. 找到所有数组中消失的数字.cpp
@@ -1,13 +1,13 @@
-\\
-\\ for(const int & num: nums) 迭代容器中所有的元素，每一个元素的临时名字为num
-\\
+//
+// for(const int & num: nums) 迭代容器中所有的元素，每一个元素的临时名字为num
+//
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> ans;
         int n=nums.size();
-        for(const int & num: nums){    \\若需要对序列中的元素进行写操作，则需要声明成引用类型 &
-            int pos = (num-1)%n;       \\对应位置的数+n之后再整除n，不影响在数组中的pos
+        for(const int & num: nums){    //若需要对序列中的元素进行写操作，则需要声明成引用类型 &
+            int pos = (num-1)%n;       //对应位置的数+n之后再整除n，不影响在数组中的pos
             nums[pos] += n;
         }
         for(int i=0;i<n;++i){
@@ -25,9 +25,9 @@ public:
         vector<int> ans;
         int n=nums.size();
         for(const int & num: nums){
-            int pos = abs(num)-1;    \\需要取绝对值，数组nums中可能存在重复的数字，不加abs数组访问会越界
+            int pos = abs(num)-1;    //需要取绝对值，数组nums中可能存在重复的数字，不加abs数组访问会越界
             if(nums[pos]>0){
-                nums[pos] = -nums[pos];   \\取负号也不影响在数组中的下标
+                nums[pos] = -nums[pos];   //取负号也不影响在数组中的下标
             }
         }
         for(int i=0;i<n;++i){
