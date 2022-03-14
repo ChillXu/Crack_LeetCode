b3a463f28a6c57cea2dacf1220a4c272489a5970
ChillXu <1587440752@qq.com>
2022-03-15 00:14:44 +0800

    Update 215. 数组中的第K个最大元素.cpp

--- Algorithm/排序算法/215. 数组中的第K个最大元素.cpp
+++ Algorithm/排序算法/215. 数组中的第K个最大元素.cpp
@@ -1,6 +1,6 @@
-\\
-\\快速选择，利用快排找到第k大的pivot即可
-\\
+//
+//快速选择，利用快排找到第k大的pivot即可
+//
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -18,7 +18,7 @@ public:
                     left=mid+1;
                 }
         }
-        return nums[left];  \\left==right时，若还没找到，则所求即为nums[left]
+        return nums[left];  //left==right时，若还没找到，则所求即为nums[left]
     }
     int quickSelection(vector<int>& nums, int left, int right){
         int i=left+1, j=right;
@@ -29,7 +29,7 @@ public:
             while(j>left && nums[j]>=nums[left]){
                 --j;
             }
-            if(i>=j){break;}        \\ while(i<j){}的情况下，不满足条件的i、j会再执行一次swap
+            if(i>=j){break;}        // while(i<j){}的情况下，不满足条件的i、j会再执行一次swap
             swap(nums[i],nums[j]);
         }
         swap(nums[left],nums[j]);
