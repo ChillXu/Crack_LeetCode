7dc28a7e830a540d3999506f15e4421d46545e66
ChillXu <1587440752@qq.com>
2022-03-14 16:44:38 +0800

    Update 695. 岛屿的最大面积.cpp

--- Algorithm/优先搜索/695. 岛屿的最大面积.cpp
+++ Algorithm/优先搜索/695. 岛屿的最大面积.cpp
@@ -1,3 +1,26 @@
 \\
+\\简单的dfs
 \\
-\\
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea=0;
+        if(grid.empty()||grid[0].empty()){  //[[]]
+            return 0;
+        }
+        for(int i=0;i<grid.size();++i){
+            for(int j=0;j<grid[0].size();++j){
+                maxArea=max(maxArea,dfs(grid,i,j));
+            }
+        }
+        return maxArea;
+    }
+    
+    int dfs(vector<vector<int>>& grid, int r, int c){
+        if(r<0||c<0||r>=grid.size()||c>=grid[0].size()||grid[r][c]==0){
+            return 0;
+        }
+        grid[r][c]=0;//搜索到的地方设定值为0，避免重复搜索
+        return 1+dfs(grid,r+1,c)+dfs(grid,r-1,c)+dfs(grid,r,c+1)+dfs(grid,r,c-1);
+    }
+};
