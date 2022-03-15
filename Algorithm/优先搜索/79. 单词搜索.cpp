7cfccbb16d35c184349de8f5bd15e6815a4ebcf4
ChillXu <1587440752@qq.com>
2022-03-15 17:04:43 +0800

    Update 79. 单词搜索.cpp

--- Algorithm/优先搜索/79. 单词搜索.cpp
+++ Algorithm/优先搜索/79. 单词搜索.cpp
@@ -1,3 +1,39 @@
 //
+//回溯法
 //
-//
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if(board.empty()){
+            return false;
+        }
+        int m=board.size(),n=board[0].size();
+        bool find=false;
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                backtracking(i,j,board,word,0,visited,find);
+            }
+        }
+        return find;
+    }
+
+    void backtracking(int i, int j, vector<vector<char>>& board, string word, int pos, vector<vector<bool>>& visited, bool& find){   //find 引用传递很关键！！！
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size()){  //边界判定
+            return;
+        }
+        if(word[pos]!=board[i][j] || visited[i][j]){  //字符不匹配或者已经使用过
+            return;
+        }
+        if(pos==word.size()-1){  //上述都不满足且是最后一个字符，则说明网格中存在该字符串
+            find=true;
+            return;
+        }
+        visited[i][j]=true;
+        backtracking(i+1,j,board,word,pos+1,visited,find);
+        backtracking(i-1,j,board,word,pos+1,visited,find);
+        backtracking(i,j+1,board,word,pos+1,visited,find);
+        backtracking(i,j-1,board,word,pos+1,visited,find);
+        visited[i][j]=false;   //回溯
+    }
+};
