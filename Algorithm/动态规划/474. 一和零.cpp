769f83fafc9e5df8c83e7dbb70fe44a819b1d3d9
ChillXu <1587440752@qq.com>
2022-03-21 00:14:07 +0800

    Update 474. 一和零.cpp

--- Algorithm/动态规划/474. 一和零.cpp
+++ Algorithm/动态规划/474. 一和零.cpp
@@ -1,3 +1,30 @@
 //
+//多维费用的 0-1 背包问题
+//需要空间压缩
 //
-//
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(const string& s:strs){
+            auto [c0,c1]=count(s);
+            for(int i=m;i>=c0;--i){        //逆向遍历
+                for(int j=n;j>=c1;--j){
+                    dp[i][j]=max(dp[i][j],dp[i-c0][j-c1]+1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    pair<int,int> count(const string& s){     //或者直接调用容器的count函数，count(strs.begin(),strs.end(),'0')
+        int c0=s.length(),c1=0;
+        for(const char& c:s){
+            if(c=='1'){
+                --c0;
+                ++c1;
+            }
+        }
+        return make_pair(c0,c1);
+    }
+};
