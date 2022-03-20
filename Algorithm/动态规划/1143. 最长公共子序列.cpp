a9e8f5e30d3cbf923c7da52415d8ac363ff354fa
ChillXu <1587440752@qq.com>
2022-03-20 22:27:14 +0800

    Update 1143. 最长公共子序列.cpp

--- Algorithm/动态规划/1143. 最长公共子序列.cpp
+++ Algorithm/动态规划/1143. 最长公共子序列.cpp
@@ -1,3 +1,22 @@
 //
+//dp[i][j]表示到第一个字符串位置i、到第二个字符串位置j为止，最长的公共子序列长度
+//两层遍历，全部更新
 //
-//
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.length(),n=text2.length();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;++i){
+            for(int j=1;j<=n;++j){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
