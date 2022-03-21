ed0fed7234f327596e0e828b2cf151900e921415
ChillXu <1587440752@qq.com>
2022-03-21 17:44:50 +0800

    Update 650. 只有两个键的键盘.cpp

--- Algorithm/动态规划/650. 只有两个键的键盘.cpp
+++ Algorithm/动态规划/650. 只有两个键的键盘.cpp
@@ -7,7 +7,7 @@ public:
         vector<int> dp(n+1);
         for(int i=2;i<=n;++i){
             dp[i]=i;           //若dp[i]不能被整除，dp[i]=i，dp[2]=2(先复制全部，后粘贴一次)
-            for(int j=2;j*j<=i;++j){
+            for(int j=2;j*j<=i;++j){     //超过sqrt(i)，j与i/j会重复
                 if(i%j==0){
                     dp[i]=min(dp[i],dp[j]+dp[i/j]);  //若能被j整除，操作次数等价于把一个长度为1的A延展到长度为i/j（两边同除i）
                 }
