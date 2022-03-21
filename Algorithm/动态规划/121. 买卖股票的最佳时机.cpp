822e1c6136af387a3dd0920fd78f9755098abbf3
ChillXu <1587440752@qq.com>
2022-03-21 22:17:15 +0800

    Update 121. 买卖股票的最佳时机.cpp

--- Algorithm/动态规划/121. 买卖股票的最佳时机.cpp
+++ Algorithm/动态规划/121. 买卖股票的最佳时机.cpp
@@ -1,3 +1,16 @@
 //
+//暴力解法，双重循环会超时
+//只遍历一遍，维护两个值，当前最低价格和当前最大收益
 //
-//
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxprofit=0,minprice=INT_MAX;
+        for(int i=0;i<n;++i){
+            minprice=min(minprice,prices[i]);
+            maxprofit=max(maxprofit,prices[i]-minprice);
+        }
+        return maxprofit;
+    }
+};
