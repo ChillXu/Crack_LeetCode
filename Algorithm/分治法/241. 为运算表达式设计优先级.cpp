8a02a49826aa8442c8b38e825663400523a6c41e
ChillXu <1587440752@qq.com>
2022-03-22 22:57:00 +0800

    Update 241. 为运算表达式设计优先级.cpp

--- Algorithm/分治法/241. 为运算表达式设计优先级.cpp
+++ Algorithm/分治法/241. 为运算表达式设计优先级.cpp
@@ -27,3 +27,46 @@ public:
         return ways;
     }
 };
+
+//
+//用memoization处理重复出现的子字符串
+//
+class Solution {
+public:
+    unordered_map<string,vector<int>> hash;
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> ways;
+        for(int i=0;i<expression.size();++i){
+            char c=expression[i];
+            if(c=='+' || c=='-' || c=='*'){
+                vector<int> left,right;
+                if(!hash.count(expression.substr(0,i))){         //先判断是否不在hash表中！！（这样比反过来快，说明大部分都是不在的）
+                    left=diffWaysToCompute(expression.substr(0,i));
+                }
+                else{
+                    left=hash[expression.substr(0,i)];
+                }
+                if(!hash.count(expression.substr(i+1))){
+                    right=diffWaysToCompute(expression.substr(i+1));
+                }
+                else{
+                    right=hash[expression.substr(i+1)];
+                }
+                for(const int& l:left){
+                    for(const int& r:right){
+                        switch(c){
+                            case '+': ways.push_back(l+r);break;
+                            case '-': ways.push_back(l-r);break;
+                            case '*': ways.push_back(l*r);break;
+                        }
+                    }
+                }
+            }
+        }
+        if(ways.empty()){
+            ways.push_back(stoi(expression));
+        }
+        hash[expression]=ways;
+        return ways;
+    }
+};
