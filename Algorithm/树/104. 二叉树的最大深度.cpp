8f3bd20f8c81d6b089f73734b18e9a8b1d8abdb5
ChillXu <1587440752@qq.com>
2022-03-24 21:50:44 +0800

    Update 104. 二叉树的最大深度.cpp

--- Algorithm/树/104. 二叉树的最大深度.cpp
+++ Algorithm/树/104. 二叉树的最大深度.cpp
@@ -1,3 +1,20 @@
-//
-//
-//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        return 1+max(maxDepth(root->left),maxDepth(root->right));  //二叉树的最大深度 = 其左子树和右子树的最大深度 + 1
+    }
+};
