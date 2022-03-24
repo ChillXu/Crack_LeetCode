19a4dd32c6a8a52fd83fbc569a9b2d9a06e37017
ChillXu <1587440752@qq.com>
2022-03-24 17:49:20 +0800

    Update 160. 相交链表.cpp

--- Algorithm/链表/160. 相交链表.cpp
+++ Algorithm/链表/160. 相交链表.cpp
@@ -1,3 +1,23 @@
 //
+//先遍历headA，用unordered_set记录所有节点
+//再遍历headB，查找节点是否在表中存在
 //
-//
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> visited;
+        ListNode *temp=headA;
+        while(temp){
+            visited.insert(temp);
+            temp=temp->next;
+        }
+        temp=headB;
+        while(temp){
+            if(visited.count(temp)){
+                return temp;
+            }
+            temp=temp->next;
+        }
+        return nullptr;
+    }
+};
