82a3e534debfb666ea6a66815fde71c570c34146
ChillXu <1587440752@qq.com>
2022-03-24 18:01:05 +0800

    Update 160. 相交链表.cpp

--- Algorithm/链表/160. 相交链表.cpp
+++ Algorithm/链表/160. 相交链表.cpp
@@ -21,3 +21,20 @@ public:
         return nullptr;
     }
 };
+
+//
+//假设链表 headA 的不相交部分有 a 个节点，链表 headB 的不相交部分有 b 个节点，两个链表相交的部分有 c 个节点
+//两个链表相交：1)若a=b，则在遍历完之前就会退出循环；2）若a!=b，则两指针会在遍历完 a+b+c 个节点后在相交处相遇
+//两个链表不相交：1）若a=b，则在遍历完之后会退出，此时l1=l2=nullptr；2）若a!=b，则两指针会在遍历完 a+b 个节点后都指向nullptr             
+//
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *l1=headA,*l2=headB;
+        while(l1!=l2){
+            l1=l1?l1->next:headB;
+            l2=l2?l2->next:headA;
+        }
+        return l1;
+    }
+};
