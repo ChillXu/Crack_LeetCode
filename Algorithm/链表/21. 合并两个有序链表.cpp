0f59ea41d879a4e91605c6a92851409e51dcd63c
ChillXu <1587440752@qq.com>
2022-03-24 16:28:13 +0800

    Update 21. 合并两个有序链表.cpp

--- Algorithm/链表/21. 合并两个有序链表.cpp
+++ Algorithm/链表/21. 合并两个有序链表.cpp
@@ -1,3 +1,33 @@
 //
 //
 //
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy=new ListNode();
+        ListNode* node=dummy;       //最终需要返回头节点，所以另创建一个指针用来合并
+        while(list1 && list2){
+            if(list1->val<=list2->val){
+                node->next=list1;
+                list1=list1->next;
+            }
+            else{
+                node->next=list2;
+                list2=list2->next;
+            }
+            node=node->next;
+        }
+        node->next=list1?list1:list2;
+        return dummy->next;
+    }
+};
