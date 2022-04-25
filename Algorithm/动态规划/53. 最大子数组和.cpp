a8add0aedecfb88b884cf21f606ecd0d667be20b
ChillXu <1587440752@qq.com>
2022-04-25 11:37:51 +0800

    Update 53. 最大子数组和.cpp

--- Algorithm/动态规划/53. 最大子数组和.cpp
+++ Algorithm/动态规划/53. 最大子数组和.cpp
@@ -1,5 +1,5 @@
 //
-//dp[i]为以nums[i]结尾的数组的最大子树组和
+//dp[i]为以nums[i]结尾的子数组的最大和
 //
 class Solution {
 public:
@@ -11,5 +11,22 @@ public:
             dp[i]=max(nums[i],dp[i-1]+nums[i]);
         }
         return *max_element(dp.begin(),dp.end());
+    }
+};
+
+//
+//用pre_max记录以nums[i]结尾的子数组的最大和，ans记录最大和
+//
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int pre_max=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<n;++i){
+            pre_max=max(nums[i],pre_max+nums[i]);
+            ans=max(pre_max,ans);
+        }
+        return ans;
     }
 };
