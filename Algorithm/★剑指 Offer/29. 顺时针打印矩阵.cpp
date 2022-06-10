e4989362cd8baab45ef36a08954e7d450968d45f
ChillXu <1587440752@qq.com>
2022-06-10 15:38:11 +0800

    Update 29. 顺时针打印矩阵.cpp

--- Algorithm/★剑指 Offer/29. 顺时针打印矩阵.cpp
+++ Algorithm/★剑指 Offer/29. 顺时针打印矩阵.cpp
@@ -1,1 +1,30 @@
 //
+//定义directions来改变方向
+//直接先计算nextm和nextn然后进行边界判断，就不需要回退了，如果visited那么就转向
+//
+class Solution {
+public:
+    vector<int> directions{0,1,0,-1,0};
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if(matrix.size()==0 || matrix[0].size()==0){
+            return {};
+        }
+        int row=matrix.size(), column=matrix[0].size();
+        int num=row*column;        
+        vector<vector<bool>> visited(row,vector<bool>(column,false));
+        int m=0,n=0;
+        int k=0;
+        vector<int> ordered(num);
+        for(int i=0;i<num;++i){
+            ordered[i]=matrix[m][n];
+            visited[m][n]=true;
+            int nextm=m+directions[k],nextn=n+directions[k+1];
+            if(nextn>=column || nextm>=row || nextn<0 || nextm<0 || visited[nextm][nextn]){
+                k=(k+1)%4;
+            }
+            m += directions[k];
+            n += directions[k+1];
+        }
+        return ordered;
+    }
+};
