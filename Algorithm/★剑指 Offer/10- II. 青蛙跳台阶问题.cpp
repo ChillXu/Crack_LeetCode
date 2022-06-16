e522a7ab68ed63e174c0b6d4f63221bc48f2497f
ChillXu <1587440752@qq.com>
2022-06-16 16:40:38 +0800

    Create 10- II. 青蛙跳台阶问题.cpp

---
+++ Algorithm/★剑指 Offer/10- II. 青蛙跳台阶问题.cpp
@@ -0,0 +1,17 @@
+//
+//
+//
+class Solution {
+public:
+    int numWays(int n) {
+        if(n==0 || n==1) return 1;
+        int MOD=1000000007;
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;++i){
+            dp[i]=(dp[i-1]+dp[i-2])%MOD;
+        }
+        return dp[n];
+    }
+};
