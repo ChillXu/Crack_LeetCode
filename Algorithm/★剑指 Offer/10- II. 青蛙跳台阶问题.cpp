86584531c173e8996cf0f047f3c1864a70b140fc
ChillXu <1587440752@qq.com>
2022-06-16 16:44:12 +0800

    Update 10- II. 青蛙跳台阶问题.cpp

--- Algorithm/★剑指 Offer/10- II. 青蛙跳台阶问题.cpp
+++ Algorithm/★剑指 Offer/10- II. 青蛙跳台阶问题.cpp
@@ -1,5 +1,5 @@
 //
-//
+//动态规划，空间复杂度O(n)
 //
 class Solution {
 public:
@@ -13,5 +13,23 @@ public:
             dp[i]=(dp[i-1]+dp[i-2])%MOD;
         }
         return dp[n];
+    }
+};
+
+//
+//动态规划，空间复杂度O(1)
+//
+class Solution {
+public:
+    int numWays(int n) {
+        if(n==0 || n==1) return 1;
+        int MOD=1000000007;
+        int p=1,q=1,r;
+        for(int i=2;i<=n;++i){
+            r=(p+q)%MOD;
+            p=q;
+            q=r;
+        }
+        return r;
     }
 };
