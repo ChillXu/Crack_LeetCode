5aa500e0bc2b24fd33b86828eb6d5b4289a0a5fe
ChillXu <1587440752@qq.com>
2022-07-08 14:09:23 +0800

    Update 13. 机器人的运动范围(dfs&bfs).cpp

--- Algorithm/★剑指 Offer/13. 机器人的运动范围(dfs&bfs).cpp
+++ Algorithm/★剑指 Offer/13. 机器人的运动范围(dfs&bfs).cpp
@@ -17,3 +17,30 @@ public:
                  + dfs(i,j+1,k,si,(j+1)%10==0?sj-8:sj+1,m,n,visited);
     }
 };
+
+
+//
+//bfs
+//利用队列，先将初始点(0,0)加入队列，出队判断是否为可达解，是则将其相邻右边和下边加入队列，否则跳过
+//
+
+class Solution {
+public:
+    int movingCount(int m, int n, int k) {
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        queue<vector<int>> que;
+        que.push({0,0,0,0});
+        int res=0;
+        while(!que.empty()){
+            vector<int> x=que.front();
+            que.pop();
+            int i=x[0],j=x[1],si=x[2],sj=x[3];
+            if(i>=m || j>=n || si+sj>k || visited[i][j]) continue;
+            visited[i][j]=true;
+            res++;
+            que.push({i+1,j,(i+1)%10==0?si-8:si+1,sj});
+            que.push({i,j+1,si,(j+1)%10==0?sj-8:sj+1});
+        }
+        return res;
+    }
+};
