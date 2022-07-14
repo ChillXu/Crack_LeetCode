eeb39006a1aebaca0f330caac0d970470f6ffcb1
ChillXu <1587440752@qq.com>
2022-07-14 09:58:13 +0800

    Update 25. 合并两个排序的链表.cpp

--- Algorithm/★剑指 Offer/25. 合并两个排序的链表.cpp
+++ Algorithm/★剑指 Offer/25. 合并两个排序的链表.cpp
@@ -26,3 +26,28 @@ public:
         return head->next;
     }
 };
+
+
+//
+//递归
+//
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1){             //先递归到l1、l2二者一方为空，从剩余的那一方开始，向前添加节点
+            return l2;
+        }
+        if(!l2){
+            return l1;
+        }
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+        else{
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+    }
+};
