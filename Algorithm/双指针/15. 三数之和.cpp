e76025f49d40c5ac6d319bf1f98f74e1c44551fa
ChillXu <1587440752@qq.com>
2022-07-19 11:31:32 +0800

    Create 15. 三数之和.cpp

---
+++ Algorithm/双指针/15. 三数之和.cpp
@@ -0,0 +1,34 @@
+//
+//排序+双指针
+//枚举的三元组 (a,b,c) 满足 a≤b≤c ，减少重复
+//
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int first=0;first<n;++first){             // 枚举a
+            if(first>0 && nums[first]==nums[first-1]){// 需要和上一次枚举的数不相同，nums[first]==nums[first+1]会越界
+                continue;
+            }
+            int third=n-1;
+            int target=-nums[first];
+            for(int second=first+1;second<third;++second){    // b,c双指针
+                if(second>first+1 && nums[second]==nums[second-1]){
+                    continue;
+                }
+                while(nums[second]+nums[third]>target && second<third){
+                    third--;
+                }
+                if(second>=third){                // 如果指针重合，随着 b 后续的增加就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                    break;
+                }
+                if(nums[second]+nums[third]==target){
+                    ans.push_back({nums[first],nums[second],nums[third]});
+                }
+            }
+        }
+        return ans;
+    }
+};
