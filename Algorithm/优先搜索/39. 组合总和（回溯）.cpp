f89be94c8e3415c0f22e7491472baa0b75c11c04
ChillXu <1587440752@qq.com>
2022-07-20 23:02:11 +0800

    Update 39. 组合总和（回溯）.cpp

--- Algorithm/优先搜索/39. 组合总和（回溯）.cpp
+++ Algorithm/优先搜索/39. 组合总和（回溯）.cpp
@@ -1,3 +1,7 @@
+//
+//[2,3,6,7] 7
+//[[2,2,3],[7]]
+//
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
@@ -5,18 +9,18 @@ public:
         vector<int> cur;
         //if(candidates.empty()) return ans;
         sort(candidates.begin(),candidates.end());
-        backtracking(candidates,ans,cur,target,0);
+        backtracking(candidates,ans,cur,target,0,0);
         return ans;
     }
 
-    void backtracking(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& cur, int target, int sum){
+    void backtracking(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& cur, int target, int sum, int index){
         if(sum==target){
             ans.push_back(cur);
             return;
         }
-        for(int i=0;sum+candidates[i]<=target;++i){
+        for(int i=index;i<candidates.size() && sum+candidates[i]<=target;++i){      // i<candidates.size()不能少，否则递归的第一层会数组访问溢出
             cur.push_back(candidates[i]);
-            backtracking(candidates,ans,cur,target,sum+candidates[i]);
+            backtracking(candidates,ans,cur,target,sum+candidates[i],i);            // index很关键，防止相同组合的重复排列，如[2,2,3]和[2,3,2]
             cur.pop_back();
         }
         return;
