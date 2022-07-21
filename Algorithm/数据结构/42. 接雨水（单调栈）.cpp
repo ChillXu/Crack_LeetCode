bb06daf920f8b48fc62d07a006bff9e2917f3faa
ChillXu <1587440752@qq.com>
2022-07-21 14:12:39 +0800

    Update 42. 接雨水（单调栈）.cpp

--- Algorithm/数据结构/42. 接雨水（单调栈）.cpp
+++ Algorithm/数据结构/42. 接雨水（单调栈）.cpp
@@ -1,6 +1,8 @@
 //
 //维护一个单调递减栈（栈尾大于栈头）
-//每次
+//如果当前遍历的元素（柱子）高度小于栈顶元素的高度，就把这个元素加入栈中；
+//如果当前遍历的元素（柱子）高度大于等于栈顶元素的高度，则栈顶出栈，并将下一个栈顶元素作为最左边的柱子，计算二者间的雨水；
+//直至栈为空或栈顶元素大于当前遍历元素，当前遍历元素入栈。
 //
 class Solution {
 public:
@@ -12,10 +14,10 @@ public:
             while(!stk.empty() && height[current]>=height[stk.top()]){
                 int top=stk.top();
                 stk.pop();
-                if(stk.empty()){    // 
+                if(stk.empty()){    // 出栈后，栈为空，左边没有柱子可以接雨水
                     break;
                 }
-                int h=min(height[stk.top()],height[current])-height[top];
+                int h=min(height[stk.top()],height[current])-height[top];       // 单调栈，height[stk.top()] 一定比 height[top] 大
                 int w=current-stk.top()-1;
                 sum += h*w;
             }
