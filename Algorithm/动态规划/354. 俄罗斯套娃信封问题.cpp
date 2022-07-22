48e399509421d9e249ccfe18a71d1a820d846389
ChillXu <1587440752@qq.com>
2022-07-22 11:35:41 +0800

    Update 354. 俄罗斯套娃信封问题.cpp

--- Algorithm/动态规划/354. 俄罗斯套娃信封问题.cpp
+++ Algorithm/动态规划/354. 俄罗斯套娃信封问题.cpp
@@ -13,12 +13,12 @@ public:
         vector<int> f={envelopes[0][1]};
         for(int i=1;i<n;++i){
             if(envelopes[i][1]>f.back()){
-                f.push_back(envelopes[i][1]);
+                f.push_back(envelopes[i][1]);               // 只记录h值
             }
             else{
-                auto it=lower_bound(f.begin(),f.end(),envelopes[i][1]);     // 找到f中比envelopes[i][1]严格小的最大元素
-                *it=envelopes[i][1];
-            }
+                auto it=lower_bound(f.begin(),f.end(),envelopes[i][1]);     // 找到f中大于等于envelopes[i][1]的第一个值并替换
+                *it=envelopes[i][1];                                        // 虽然不是最长递增子序列，但是不影响序列的长度，参考leetcode300
+            }                                                               // 后续的值大于末尾正常更新，不大于末尾继续替换，不影响序列长度。
         }
         return f.size();
     }
