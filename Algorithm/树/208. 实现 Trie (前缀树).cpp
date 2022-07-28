8b048927d32b89ce71bfc6954e969e0e477c0647
ChillXu <1587440752@qq.com>
2022-07-28 14:43:15 +0800

    Create 208. 实现 Trie (前缀树).cpp

---
+++ Algorithm/树/208. 实现 Trie (前缀树).cpp
@@ -0,0 +1,57 @@
+//
+//
+//
+class Trie {
+private:
+    bool isEnd;
+    vector<Trie*> next=vector<Trie*>(26);
+//     vector<Trie*> next;
+public:
+    Trie() {
+        isEnd=false;
+    }
+    
+//     Trie(): isEnd(false),next(26) {           // 初始化列表
+//     }
+      
+    
+    void insert(string word) {
+        Trie *node=this;
+        for(char c:word){
+            c-='a';
+            if(node->next[c]==nullptr){
+                node->next[c]=new Trie();
+            }
+            node=node->next[c];         // ×else
+        }
+        node->isEnd=true;
+    }
+    
+    bool search(string word) {
+        Trie *node=this;
+        for(char c:word){
+            c-='a';
+            node=node->next[c];
+            if(node==nullptr) return false;
+        }
+        return node->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        Trie *node=this;
+        for(char c:prefix){
+            c-='a';
+            node=node->next[c];
+            if(node==nullptr) return false;
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
