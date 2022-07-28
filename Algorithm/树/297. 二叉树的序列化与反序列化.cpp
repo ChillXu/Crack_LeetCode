4ef1eaeabdda5c989d1023cffcd08af0cfb42385
ChillXu <1587440752@qq.com>
2022-07-28 17:40:29 +0800

    Update 297. 二叉树的序列化与反序列化.cpp

--- Algorithm/树/297. 二叉树的序列化与反序列化.cpp
+++ Algorithm/树/297. 二叉树的序列化与反序列化.cpp
@@ -7,15 +7,18 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//
+//dfs
+//
 class Codec {
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root==nullptr) return "X";
+        if(root==nullptr) return "null";
         string left=serialize(root->left);
         string right=serialize(root->right);
-        return to_string(root->val) + "," + left + "," + right;
+        return to_string(root->val) + "," + left + "," + right;		// 先序遍历
     }
 
     // Decodes your encoded data to tree.
@@ -25,38 +28,26 @@ public:
         return root;
     }
 
-    // list<string> split_str(string& data) {
-    //     list<string> data_list;
-    //     int start=0,end=-1;
-    //     for(char c:data){
-    //         ++end;
-    //         if(c==','){
-    //             data_list.push_back(data.substr(start,end-start));
-    //             start=end+1;
-    //         }
-    //     }
-    //     return data_list;
-    // }
-    list<string> split_str(string& str)
-	{
-		list<string> res;
-		for (int lastpos = -1, pos = 0; pos < str.length(); pos++)
-		{
-			if (str[pos] == ',')
-			{
-				res.push_back(str.substr(++lastpos, pos - lastpos));
-				lastpos = pos;
-			}
-		}
-		return res;
-	}
+    list<string> split_str(string& data) {
+        data=data + ",";  		// 由于serialize的写法字符串最后没有 ',' 需要添加一个符号才能读取到最后一个节点值！！！
+        list<string> data_list;
+        int start=0,end=0;
+        for(char c:data){
+            if(c==','){
+                data_list.push_back(data.substr(start,end-start));
+                start=end+1;
+            }
+            ++end;
+        }
+        return data_list;
+    }
 
     TreeNode* buildTree(list<string>& data_list){
         string node=data_list.front();
         data_list.pop_front();
-        if(node=="X") return nullptr;
+        if(node=="null") return nullptr;
         TreeNode *root=new TreeNode(stoi(node));
-        root->left=buildTree(data_list);
+        root->left=buildTree(data_list);	// data_list一直在pop，列表还是有变化的
         root->right=buildTree(data_list);
         return root;
     }
