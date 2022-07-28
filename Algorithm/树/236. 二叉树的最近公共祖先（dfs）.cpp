dea50226cc1d834f82e7f823b30fa6503f40f2f6
ChillXu <1587440752@qq.com>
2022-07-28 15:26:14 +0800

    Update 236. 二叉树的最近公共祖先（dfs）.cpp

--- Algorithm/树/236. 二叉树的最近公共祖先（dfs）.cpp
+++ Algorithm/树/236. 二叉树的最近公共祖先（dfs）.cpp
@@ -24,3 +24,13 @@ public:
         return root;                                          // 左右子树均不为空，则root就是最近公共祖先
     }
 };
+
+
+/*
+1. 当 left 和 right 同时为空，说明 root 的左/右子树中都不包含 p,q ，返回 null ；
+2. 当 left 和 right 同时不为空，说明 p,q 分列在 root 的异侧（分别在左/右子树），因此 root 为最近公共祖先，返回 root ；
+3. 当 left 为空，right 不为空，p,q 都不在 root 的左子树中，直接返回 right 。具体可分为两种情况：
+           p,q 其中一个在 root 的右子树中，此时 right 指向 p（假设为 p ）；
+           p,q 两节点都在 root 的右子树中，此时的 right 指向最近公共祖先节点 ；
+4. 当 left 不为空， ight 为空，与情况 3. 同理。
+*/
