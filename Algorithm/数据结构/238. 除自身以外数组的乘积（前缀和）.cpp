bbce2ed0f8a9e330ae61b9db43031634166b99b0
ChillXu <1587440752@qq.com>
2022-07-29 09:52:37 +0800

    Create 238. 除自身以外数组的乘积（前缀和）.cpp

---
+++ Algorithm/数据结构/238. 除自身以外数组的乘积（前缀和）.cpp
@@ -0,0 +1,21 @@
+//
+//乘积 = 当前数左边的乘积 * 当前数右边的乘积
+//时间：O(n)  空间：O(1)  （输出数组不被视为额外空间）
+//
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        ans[0]=1;
+        int k=1;
+        for(int i=1;i<n;++i){
+            ans[i]=ans[i-1]*nums[i-1];    // ans[i]表示当前数nums[i]左边的乘积，即先计算前缀和
+        }
+        for(int i=n-1;i>=0;--i){
+            ans[i]*=k;
+            k*=nums[i];
+        }
+        return ans;
+    }
+};
