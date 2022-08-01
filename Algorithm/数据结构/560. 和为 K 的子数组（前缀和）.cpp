48c4acdbeb9c9039e2d32dd0a621b97ba174d174
ChillXu <1587440752@qq.com>
2022-08-01 17:02:23 +0800

    Update 560. 和为 K 的子数组（前缀和）.cpp

--- Algorithm/数据结构/560. 和为 K 的子数组（前缀和）.cpp
+++ Algorithm/数据结构/560. 和为 K 的子数组（前缀和）.cpp
@@ -18,3 +18,28 @@ public:
         return cnt;
     }
 };
+
+
+
+//
+//将和为K转化为前缀和之差为K，若[j,i]的和为K，则prefix[i]-prefix[j-1]=K
+//所以遍历nums计算prefix[i]，判断prefix[i]-K是否在哈希表中，若存在则和为K的子数组存在
+//
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int cnt=0;
+        int prefix=0;
+        unordered_map<int,int> hash;    // 哈希表记录已经计算的前缀和
+        hash[0]=1;
+        for(int i=0;i<n;++i){
+            prefix+=nums[i];
+            if(hash.count(prefix-k)){
+                cnt+=hash[prefix-k];
+            }
+            hash[prefix]++;         // 可能存在不同的i但是前缀和相同的情况，所以需要累加
+        }
+        return cnt;
+    }
+};
