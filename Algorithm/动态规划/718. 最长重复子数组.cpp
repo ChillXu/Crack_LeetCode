83be2781c1a79129f10c3ea4e0d9150f165c1bb0
ChillXu <1587440752@qq.com>
2022-08-11 14:53:14 +0800

    Create 718. 最长重复子数组.cpp

---
+++ Algorithm/动态规划/718. 最长重复子数组.cpp
@@ -0,0 +1,26 @@
+//
+//题目：给两个整数数组 nums1 和 nums2 ，返回 两个数组中公共的、长度最长的子数组的长度。
+//
+//令 dp[i][j] 表示 A[i:] 和 B[j:] 的最长公共前缀，
+//如果 A[i] == B[j]，那么我们知道 A[i:] 与 B[j:] 的最长公共前缀为 A[i + 1:] 与 B[j + 1:] 的最长公共前缀的长度加一，
+//否则 A[i:] 与 B[j:] 的最长公共前缀为零,（从后向前遍历更新）
+//那么答案即为所有 dp[i][j] 中的最大值。
+//
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size(),n=nums2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        int ans=0;
+        for(int i=m-1;i>=0;--i){
+            for(int j=n-1;j>=0;--j){
+                if(nums1[i]==nums2[j]){
+                    dp[i][j]=dp[i+1][j+1]+1;
+                }
+                else dp[i][j]=0;
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
