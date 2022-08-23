5e37c791cdd5d3afe2b49308a841da53e11199f9
ChillXu <1587440752@qq.com>
2022-08-23 09:46:07 +0800

    Update 152. 乘积最大子数组.cpp

--- Algorithm/动态规划/152. 乘积最大子数组.cpp
+++ Algorithm/动态规划/152. 乘积最大子数组.cpp
@@ -1,10 +1,14 @@
+//
+//题目：给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+//maxF[i]表示以第 ii 个元素结尾的乘积最大子数组的乘积
+//
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        vector <int> maxF(nums), minF(nums);
+        vector <int> maxF(nums), minF(nums);            // 注意初始化！！！
         for (int i = 1; i < nums.size(); ++i) {
             maxF[i] = max(maxF[i - 1] * nums[i], max(nums[i], minF[i - 1] * nums[i]));
-            minF[i] = min(minF[i - 1] * nums[i], min(nums[i], maxF[i - 1] * nums[i]));
+            minF[i] = min(minF[i - 1] * nums[i], min(nums[i], maxF[i - 1] * nums[i]));      // 需要同时维护一个minF
         }
         return *max_element(maxF.begin(), maxF.end());
     }
